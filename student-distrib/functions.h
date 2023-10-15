3a4cf597694cd6ea755ae483ee1cfd66cd6416de
prarthik krishnan <prarthik2003@gmail.com>
2023-10-15 02:52:15 -0500

    idt might be done? RTC and keyboard interrupts left to do

--- student-distrib/functions.h
+++ student-distrib/functions.h
@@ -1,9 +1,18 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-void DIVZERO();
-void DEBUG_EX();
-void NMI_INT();
-
-
-#endif FUNCTIONS_H
+void diverror() ; 
+void debug() ; 
+void nmi() ; 
+void breakpoint() ; 
+void overflow() ; 
+void boundrange() ; 
+void invalidop() ; 
+void device_na() ; 
+void doublefault() ; 
+void invalidtss() ; 
+void segment_na() ; 
+void stack_seg() ; 
+void general_protection() ; 
+void page_fault() ; 
+void floating_excep() ; 
+void alignment_check() ; 
+void machine_check() ; 
+void smd() ; 
---
+++ student-distrib/script.py
@@ -0,0 +1,12 @@
+arr = [line.split() for line in open('functions.c').read().split('\n')]
+arr = [com for com in arr if com != [] and com[0] == 'void']
+for i in range(len(arr)):
+    arr[i].pop()
+    temp = list(arr[i][1])
+    temp.pop()
+    temp.pop()
+    arr[i][1] = ''.join(temp)
+    arr[i].append('_asm')
+    del arr[i][0]
+    arr[i] = ''.join(arr[i])
+    print(arr[i], end=" ")
