61d62cc84e94518b02236ac09a9b6302483ec2e3
prarthik krishnan <prarthik2003@gmail.com>
2023-10-21 21:35:10 -0500

    rtc functions for system calls

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -2,6 +2,11 @@
 #include "lib.h"
 #include "i8259.h"
 
+// private vars used here
+static unsigned long rtc_rate;
+static unsigned long tally;
+static unsigned long global_count;
+static unsigned long return_flag_read;
 
 /* init_rtc
  * 
@@ -17,7 +22,14 @@ void init_rtc() {
     char prev = inb(0x71); // read value at reg b
     outb(0x8b, 0x70); // set index
     outb(prev | 0x40, 0x71); // return the flag values read by oring and writing to port
+
+    // set rtc to highest frequency possible that doesn't glitch in the emulation (1024)
+    rtc_rate = 6;
+    outb(0x8a, 0x70);
+    outb(rtc_rate, 0x71);
     enable_irq((uint32_t) 8); // call the enable function to set the interrupt on the pic
+    global_count = 0;
+    
 }
 /* init_rtc
  * 
@@ -29,7 +41,14 @@ void init_rtc() {
  * Files: i8259.c/h
  */
 void rtc_handler(){
-    //test_interrupts();
+    if(global_count < tally){
+        global_count ++;
+    }
+    else{
+        // test_interrupts(); // whatever the rtc handler is supposed to do
+        global_count = 0;
+        return_flag_read = 1;
+    }
     cli(); // protect from interrupts
     outb(0x0c, 0x70); // read contents into register (0x0c)
     inb(0x71); // throw contents away
@@ -37,3 +56,34 @@ void rtc_handler(){
     sti(); // re enable interrupts
 }
 
+void rtc_freq(int32_t freq){
+    if(freq < base_freq){
+        tally = base_freq / freq;
+    }
+}
+
+int32_t rtc_open(const uint8_t* filename){
+    rtc_freq(2);
+    return 0;
+}
+
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes){
+    return_flag_read = 0;
+    while(!return_flag_read);
+    return 0;
+}
+
+int32_t rtc_write((int32_t fd, const void* buf, int32_t nbytes){
+    if(buf == NULL) return -1;
+    int32_t* freq = (int32_t*)buf;
+    int32_t val = *freq;
+    if(val & (val - 1) == 0) return -1; // not sure here
+    cli();
+    rtc_freq(val);
+    sti();
+    return 0;
+}
+
+int32_t rtc_close(int32_t fd){
+    return 0;
+}
